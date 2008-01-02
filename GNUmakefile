ebb65e4e9d40023aa7b56c7427709e73f0753c05
rob <rob>
2008-01-02 19:53:39 +0000

    ''

---
+++ GNUmakefile
@@ -0,0 +1,15 @@
+.PHONY: all test time clean distclean dist distcheck upload distupload
+
+all: test
+
+dist distclean test tardist: Makefile
+	make -f $< $@
+
+Makefile: Makefile.PL
+	perl $<
+
+clean: distclean
+
+reset: clean
+	perl Makefile.PL
+	make -f Makefile test
---
+++ lib/File/Assets.pm
@@ -0,0 +1,107 @@
+package File::Assets;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+File::Assets - The great new File::Assets!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use File::Assets;
+
+    my $foo = File::Assets->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-file-assets at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-Assets>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::Assets
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-Assets>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/File-Assets>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/File-Assets>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/File-Assets>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Robert Krimen, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of File::Assets
